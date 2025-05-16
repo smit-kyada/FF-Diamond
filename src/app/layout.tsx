aaccfae52d06709ae7dc1778236a1a38e17391af
Smit Kyada <smit.cuelix@gmail.com>
2025-05-16 15:58:11 +0530

    ad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,13 +22,6 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/resources/Diamond.png" />
         <meta name="theme-color" content="#0d6efd" />
 
-        {/* AdSense Script */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8015558724608474"
-          crossOrigin="anonymous"
-        ></script>
-
         {/* Neu Icons CSS */}
         <link
           rel="stylesheet"
@@ -39,26 +32,19 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K2G4VSVX');
+            <script type="text/javascript">
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "rkezplhlom");
+            </script>
           `,
           }}
         />
       </head>
       <body className="body-bg">
-        {/* Google Tag Manager noscript */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K2G4VSVX"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
+        
         {children}
 
         {/* Toastify container (optional) */}
