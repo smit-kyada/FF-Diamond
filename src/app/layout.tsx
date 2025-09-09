ee43a0e222b05948e6887dc86ef0215df1d61a76
Smit Kyada <smit.cuelix@gmail.com>
2025-09-09 15:48:06 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,22 @@ export default function RootLayout({
           href="https://cdn.jsdelivr.net/npm/neuicons/css/neu.min.css"
         />
 
+        {/* Google Analytics (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-L7LWYD1HES"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-L7LWYD1HES');
+            `,
+          }}
+        />
+
         {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
@@ -36,7 +52,7 @@ export default function RootLayout({
                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "rkezplhlom");
+                })(window, document, "clarity", "script", "t80iwc51ct");
           `,
           }}
         />
